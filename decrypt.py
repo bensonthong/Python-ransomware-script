137390021f55ff4f64ddf1eeddb15eeab0a77a3b
Benson <thongbenson010499@gmail.com>
2022-06-16 00:49:28 -0700

    Added comments

--- decrypt.py
+++ decrypt.py
@@ -2,6 +2,10 @@ import os
 from cryptography.fernet import Fernet
 
 files = []
+# ========================================================= #
+# -Grab file in current directory and append to a list      #
+# -Filtering python files, created files, and folders       #
+# ========================================================= #
 for file in os.listdir():
     if file == "encrypt.py" or file == "key.key" or file == "decrypt.py":
         continue
@@ -10,18 +14,28 @@ for file in os.listdir():
         files.append(file)
 print(files)
 
+# ========================================================= #
+# Open the key.key file to read they encryption             #
+# ========================================================= #
 with open("key.key", "rb") as k:
     secretkey = k.read()
 
+# ========================================================= #
+# Creating password and ask user for password to decrypt    #
+# ========================================================= #
 password = "notpassword"
 user_phrase = input("Enter password to decrypt: \n")
+
+# ========================================================= #
+# Check if password is successful, then decrypts all files  #
+# ========================================================= #
 if user_phrase == password:
     for file in files:
         with open(file, "rb") as f:
             contents = f.read()
-        contents_decrypted = Fernet(secretkey).decrypt(contents)  # encrypting the contents generated for each file
+        contents_decrypted = Fernet(secretkey).decrypt(contents)  # decrypting the contents generated for each file
         with open(file, "wb") as f:
-            f.write(contents_decrypted)                     # write it back as an encrypted file
+            f.write(contents_decrypted)                           # write it back as a decrypted
     print("Files are decrypted!")
 else:
     print("Wrong password!")
