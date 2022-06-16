a2f66d3447ed3727b67315d3ebb1892fdb9bcf99
Benson <thongbenson010499@gmail.com>
2022-06-16 00:29:51 -0700

    added a password to decrypt

--- decrypt.py
+++ decrypt.py
@@ -13,9 +13,15 @@ print(files)
 with open("key.key", "rb") as k:
     secretkey = k.read()
 
-for file in files:
-    with open(file, "rb") as f:
-        contents = f.read()
-    contents_decrypted = Fernet(secretkey).decrypt(contents)  # encrypting the contents generated for each file
-    with open(file, "wb") as f:
-        f.write(contents_decrypted)                     # write it back as an encrypted file
+password = "notpassword"
+user_phrase = input("Enter password to decrypt: \n")
+if user_phrase == password:
+    for file in files:
+        with open(file, "rb") as f:
+            contents = f.read()
+        contents_decrypted = Fernet(secretkey).decrypt(contents)  # encrypting the contents generated for each file
+        with open(file, "wb") as f:
+            f.write(contents_decrypted)                     # write it back as an encrypted file
+    print("Files are decrypted!")
+else:
+    print("Wrong password!")
