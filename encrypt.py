2389b88ed7375ee11f438bed514de26fcdafc953
Benson <thongbenson010499@gmail.com>
2022-06-15 22:55:31 -0700

    added cryptography lib, generated key using the lib, created a file .key and put key inside

--- encrypt.py
+++ encrypt.py
@@ -1,8 +1,19 @@
 import os
+from cryptography.fernet import Fernet
 
 files = []
 for file in os.listdir():
-    if file == "encrypt.py":
+    if file == "encrypt.py" or file == "key.key":
         continue
-    files.append(file)
+    # This checks to see if the file in the directory is a file.
+    if os.path.isfile(file):
+        files.append(file)
 print(files)
+
+key = Fernet.generate_key()
+
+# Creates a file .key and call that object the key. Can write binary.
+# After open, will write the saved key value into the file.
+with open("key.key", "wb") as thekey:
+    thekey.write(key)
+
